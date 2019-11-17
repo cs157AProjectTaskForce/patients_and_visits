231999b2eaa24cc7a7645ddeb30f19dc73eb3a63
LeoDAnda18 <leonel_deanda@yahoo.com>
2019-11-16 19:43:33 -0800

    Finished a rough draft of Req 1.2 UI.

--- Menu.java
+++ Menu.java
@@ -8,7 +8,6 @@ public class Menu {
 			// TODO Auto-generated constructor stub
 			JFrame Screentitle = new JFrame("eTRT");
 			displayMain(Screentitle);
-
 		}
 
 		private static void displayMain(JFrame frame)
@@ -45,15 +44,21 @@ public class Menu {
 			frame.getContentPane().removeAll();
 			frame.setSize(500, 250);
 			
-			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 40));
+			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 20));
 			
 			JButton AddPatient = new JButton("Add New Patient");
 			AddPatient.setPreferredSize(new Dimension(200, 100));
+			AddPatient.addActionListener(event -> addNewPatient(frame));
 			panel.add(AddPatient);
 			
 			JButton ViewEditPatient = new JButton("View/Edit Patient");
 			ViewEditPatient.setPreferredSize(new Dimension(200, 100));
 			panel.add(ViewEditPatient);
+			
+			JButton GoBack = new JButton("Go Back");
+			GoBack.setPreferredSize(new Dimension(100, 50));
+			GoBack.addActionListener(event -> displayMain(frame));
+			panel.add(GoBack);
 			
 	        frame.setContentPane(panel);
 			frame.setVisible(true);
@@ -65,7 +70,7 @@ public class Menu {
 			frame.getContentPane().removeAll();
 			frame.setSize(500, 250);
 			
-			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 40));
+			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 30, 20));
 			
 			JButton AddNewVisit = new JButton("Add New Visit");
 			AddNewVisit.setPreferredSize(new Dimension(200, 100));
@@ -75,7 +80,131 @@ public class Menu {
 			ViewEditVisit.setPreferredSize(new Dimension(200, 100));
 			panel.add(ViewEditVisit);
 			
+			JButton GoBack = new JButton("Go Back");
+			GoBack.setPreferredSize(new Dimension(100, 50));
+			GoBack.addActionListener(event -> displayMain(frame));
+			panel.add(GoBack);
+			
 	        frame.setContentPane(panel);
 			frame.setVisible(true);
+		}
+		
+		private static void addNewPatient(JFrame frame)
+		{
+			frame.setTitle("Add Patient");
+			frame.getContentPane().removeAll();
+			frame.setSize(700, 450);
+			
+			JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 20));
+			JPanel demoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 20));
+			
+			setPanel(panel, frame);
+			setDemoPanel(demoPanel, frame);
+			
+			JButton Save = new JButton("Save");
+			Save.setPreferredSize(new Dimension(100, 30));
+			Save.addActionListener(event -> {
+				//save the data to SQL database
+				//SQLSend();
+				displayPatient(frame);
+			});
+			panel.add(Save);
+			
+			JButton demoSave = new JButton("Save");
+			demoSave.setPreferredSize(new Dimension(100, 30));
+			demoSave.addActionListener(event -> {
+				//save the data to SQL database
+				//SQLSend();
+				displayPatient(frame);
+			});
+			demoPanel.add(demoSave);
+			
+			JButton addDemographics = new JButton("Add Demographics");
+			addDemographics.setPreferredSize(new Dimension(100, 30));
+			addDemographics.addActionListener(event -> {
+				frame.setContentPane(demoPanel);
+				frame.setVisible(true);
+			});
+			panel.add(addDemographics);
+			
+			JButton back = new JButton("Back");
+			back.setPreferredSize(new Dimension(100, 30));
+			back.addActionListener(event -> {
+				frame.setContentPane(panel);
+				frame.setVisible(true);
+			});
+			demoPanel.add(back);
+			
+			JButton newVisit = new JButton("New Visit");
+			newVisit.setPreferredSize(new Dimension(100, 30));
+			newVisit.addActionListener(event -> displayPatient(frame));
+			panel.add(newVisit);
+			
+			JButton demoNewVisit = new JButton("New Visit");
+			demoNewVisit.setPreferredSize(new Dimension(100, 30));
+			demoNewVisit.addActionListener(event -> displayPatient(frame));
+			demoPanel.add(demoNewVisit);
+			
+			JButton Cancel = new JButton("Cancel");
+			Cancel.setPreferredSize(new Dimension(100, 30));
+			Cancel.addActionListener(event -> displayPatient(frame));
+			panel.add(Cancel);
+			
+			JButton demoCancel = new JButton("Cancel");
+			demoCancel.setPreferredSize(new Dimension(100, 30));
+			demoCancel.addActionListener(event -> displayPatient(frame));
+			demoPanel.add(demoCancel);
+			
+			frame.setContentPane(panel);
+			frame.setVisible(true);
+		}
+		
+		private static void addField(JPanel panel, String label)
+		{
+			JPanel temp = new JPanel();
+			JLabel tempLabel = new JLabel(label);
+			JTextField tempField = new JTextField();
+			tempField.setColumns(10);
+			temp.add(tempLabel, BorderLayout.NORTH);
+			temp.add(tempField, BorderLayout.CENTER);
+			panel.add(temp);
+		}
+		
+		private static void setPanel(JPanel panel, JFrame frame)
+		{
+			addField(panel, "First Name*");
+			addField(panel, "Last Name*");
+			addField(panel, "Middle Name");
+			addField(panel, "Date of Birth* (MM-DD-YYYY)");
+			addField(panel, "Gender* (M/F)");
+			addField(panel, "Phone Number*");
+			addField(panel, "Email");
+			addField(panel, "Street Address*");
+			addField(panel, "City*");
+			addField(panel, "State");
+			addField(panel, "Zip*");
+			addField(panel, "Country*");
+			addField(panel, "Social Security Number");
+			addField(panel, "Insurance");	
+		}
+		
+		
+		
+		private static void setDemoPanel(JPanel demoPanel, JFrame frame)
+		{
+			addField(demoPanel, "Occupation");
+			addField(demoPanel, "Work Status");
+			addField(demoPanel, "Educational Degree");
+			addField(demoPanel, "Tinnitus Onset");
+			addField(demoPanel, "Tinnitus Etiology");
+			addField(demoPanel, "Hyperacusis Onset");
+			addField(demoPanel, "Hyperacusis Etiology");
+			
+			JPanel temp = new JPanel();
+			JLabel tempLabel = new JLabel("Additional Comments");
+			JTextArea addComments = new JTextArea(10,15);
+			temp.add(tempLabel, BorderLayout.NORTH);
+			temp.add(addComments, BorderLayout.CENTER);
+			demoPanel.add(temp);
 		}
 }
