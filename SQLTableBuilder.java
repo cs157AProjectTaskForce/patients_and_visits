dc77029e6b2e5154bdf6f10b4c6fcb493ea75fc2
alvinjiang32 <36828072+alvinjiang32@users.noreply.github.com>
2019-11-25 20:39:51 -0800

    Edited tables for correct implementation

--- SQLTableBuilder.java
+++ SQLTableBuilder.java
@@ -2,12 +2,38 @@ import java.sql.*;
 
 public class SQLTableBuilder
 {
-    public static String USERNAME = new String("USER");
-    public static String PASSWORD = new String("PASS");
+    public static String USERNAME = new String("root");
+    public static String PASSWORD = new String("cs157apass");
     public static String DB_URL = new String("jdbc:mysql://localhost:3306/test");
     public static String JDBC_DRIVER = new String("com.mysql.jdbc.Driver");
     // SQL Statements to create tables, formatted as strings.
     private static String[]	Tables = {
+
+            "create table Country (" +
+                    "NAME varchar(20) not null PRIMARY KEY)"
+            ,
+
+            // zipcodes are unique
+            "create table Zip (" +
+                    "ID int(15) not null PRIMARY KEY)"
+
+            ,
+
+            "create table State (" +
+                    "NAME char(2) not null PRIMARY KEY)"
+            ,
+
+            "create table Work_Status (" +
+                    "ID int(15) not null PRIMARY KEY, " +
+                    "EMPLOYED char(3) not null)"
+            ,
+
+            "create table Occupation (" +
+                    "ID int(15) not null PRIMARY KEY, " +
+                    "JOB_NAME varchar(20) not null, " +
+                    "DEGREE varchar(20))"
+            ,
+
             "create table Patient (" +
                     "THC int(15) not null PRIMARY KEY," +
                     "DATE date not null, " +
@@ -28,9 +54,10 @@ public class SQLTableBuilder
                     "PHOTO blob(100), " +
                     "SSN int(9), " +
                     "INSURANCE varchar(30)," +
-                    "FOREIGN KEY (patient_city, patient_zip) REFERENCES Zip(ID, CITY)" +
-                    "FOREIGN KEY patient_state REFERENCES State(NAME)" +
-                    "FOREIGN KEY patient_country REFERENCES Country(NAME))"
+                    "CONSTRAINT p_zip FOREIGN KEY zip (ZIP) REFERENCES ZIP(ID), " +
+                    //"CONSTRAINT p_city FOREIGN KEY city (CITY) REFERENCES Zip(CITY), " +
+                    "CONSTRAINT p_state FOREIGN KEY state (STATE) REFERENCES State(NAME), " +
+                    "CONSTRAINT p_cty FOREIGN KEY country (COUNTRY) REFERENCES Country(NAME))"
             ,
 
             "create table Visit (" +
@@ -48,34 +75,6 @@ public class SQLTableBuilder
                     "FU varchar(20), " +
                     "COMMENTS varchar(150), " +
                     "NEXT_VISIT date not null)"
-            ,
-
-            "create table Country (" +
-                    "ID int(15) not null PRIMARY KEY, " +
-                    "NAME varchar(20) not null)"
-            ,
-
-            // zipcodes are unique
-            "create table Zip (" +
-                    "ID int(15) not null PRIMARY KEY, " +
-                    "CITY varchar(30) not null)"
-
-            ,
-
-            "create table State (" +
-                    "ID int(15) not null PRIMARY KEY, " +
-                    "NAME varchar(20) not null)"
-            ,
-
-            "create table Work_Status (" +
-                    "ID int(15) not null PRIMARY KEY, " +
-                    "EMPLOYED char(3) not null)"
-            ,
-
-            "create table Occupation (" +
-                    "ID int(15) not null PRIMARY KEY, " +
-                    "JOB_NAME varchar(20) not null, " +
-                    "DEGREE varchar(20))"
     };
 
     public static void main(String[] args) throws ClassNotFoundException
